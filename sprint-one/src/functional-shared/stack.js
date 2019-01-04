47b34f81bfd5f561dc4c139053aecd58377138ee
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-04 12:24:20 -0800

    Implement stack and queue functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  (someInstance.storage = {}), (someInstance.length = 0);
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function() {
+    var popped = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return popped;
+  },
+  size: function() {
+    return this.length > 0 ? this.length : 0;
+  }
+};
