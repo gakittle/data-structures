63da506f0f824f5904936082f5161c5b27d555df
gakittle <greg.kittleson@gmail.com>
2019-01-07 15:59:16 -0800

    Implement binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,51 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (this.value > value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.value > target) {
+    if (this.left) {
+      if (this.left.contains(target)) {
+        return true;
+      }
+    }
+  } else if (this.right) {
+    if (this.right.contains(target)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
